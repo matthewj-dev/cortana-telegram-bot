41468f2af1c4b6e8e0ddb3d44d4c32bce4ccf1b7
Matthew Jackson <14649171-mattj_dev@users.noreply.gitlab.com>
2023-06-08 13:09:18 +0000

    remove database stuff :(

---
+++ CortanaTelegramBot/Commands/BingChat.fs
@@ -0,0 +1,30 @@
+﻿module CortanaTelegramBot.Commands.BingChat
+
+open Funogram.Telegram.Bot
+open Funogram.Telegram.Types
+open Funogram.Telegram
+open Funogram.Types
+open CortanaTelegramBot.Core
+
+let askBing (ctx: UpdateContext) (config: BotConfig) (chatId: int64) (input: string): unit =
+    if ctx.Update.Message.IsNone then
+        ()
+    
+    let msg = ctx.Update.Message.Value
+
+    task {
+        let! conversation = bingClient.CreateConversation()
+        let! result = conversation.AskAsync input
+        
+        // TODO this library can not give back a conversation ID...
+        // TODO might have to maintain a singleton collection of all recent chats -> convos... so much for database...
+
+        botResult config (Api.sendMessage msg.Chat.Id result)
+        |> processResultWithValue
+        |> ignore
+    }
+    |> ignore
+
+    botResult config (Api.sendMessage msg.Chat.Id "Hold on, let me think about it...")
+    |> processResultWithValue
+    |> ignore
---
+++ CortanaTelegramBot/Program.fs
@@ -0,0 +1,17 @@
+﻿module CortanaTelegramBot.Program
+
+open Funogram.Api
+open Funogram.Telegram
+open Funogram.Telegram.Bot
+open CortanaTelegramBot.Commands.Core
+
+[<EntryPoint>]
+let main _ =
+    async {
+        let config = Config.withReadTokenFromEnv "TelegramBotToken" Config.defaultConfig 
+        let! _ = Api.deleteWebhookBase () |> api config
+        return! startBot config updateArrived None
+    }
+    |> Async.RunSynchronously
+    
+    0
