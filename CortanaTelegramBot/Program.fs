7cd5e751dc66c9ef346aec4b0c6deaa3a797d70a
Matthew Jackson <13512861+matthewj-dev@users.noreply.github.com>
2023-06-10 00:32:35 -0500

    add 5 sec interval when a top level error occurs

--- CortanaTelegramBot/Program.fs
+++ CortanaTelegramBot/Program.fs
@@ -1,17 +1,31 @@
 ﻿module CortanaTelegramBot.Program
 
+open System
 open Funogram.Api
 open Funogram.Telegram
 open Funogram.Telegram.Bot
 open CortanaTelegramBot.Commands.Core
+open CortanaTelegramBot.Core
+open Microsoft.Extensions.Logging
+open Microsoft.FSharp.Core
 
 [<EntryPoint>]
 let main _ =
-    async {
-        let config = Config.withReadTokenFromEnv "TelegramBotToken" Config.defaultConfig 
-        let! _ = Api.deleteWebhookBase () |> api config
-        return! startBot config updateArrived None
-    }
-    |> Async.RunSynchronously
     
+    let timer = new Timers.Timer(5000.)
+    let event = Async.AwaitEvent (timer.Elapsed) |> Async.Ignore
+    timer.Start()
+    
+    while true do
+        async {
+            try
+                let config = Config.withReadTokenFromEnv "TelegramBotToken" Config.defaultConfig 
+                let! _ = Api.deleteWebhookBase () |> api config
+                return! startBot config updateArrived None
+            with e ->
+                exLogger LogLevel.Error "Error asking BingChat {input}" e
+        }
+        |> Async.RunSynchronously
+        
+        Async.RunSynchronously event
     0
